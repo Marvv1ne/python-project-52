56e8f7bd1e8fbc410014af5cb3dde850f3d29592
Marvv1ne <gorn89@inbox.ru>
2025-03-25 19:01:29 +0400

    fix: remove task_manager from installed app and put main_page into it

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_page',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager',
+    
 ]
 
 MIDDLEWARE = [
