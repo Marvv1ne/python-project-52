56722ee83d11f4e29f149ff64bdf24970d721d92
Marvv1ne <gorn89@inbox.ru>
2025-03-25 19:05:53 +0400

    fix: improve path to main_page view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from main_page import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', views.index)
 ]
