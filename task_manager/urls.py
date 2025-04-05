9c9efc7183c3417e0c6457822494df184c10690c
Marvv1ne <gorn89@inbox.ru>
2025-04-05 19:22:31 +0400

    feat: almoust understand django_bootstra5 and make register form

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import main
+import main.urls
 import users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(main.urls))
+    path('', include(main.urls)),
+    path('users', include('users.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
-s
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.utils.translation import gettext_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
-def index(request):
-    return render(request, 'main_page/index.html')
+class SignUp(SuccessMessageMixin, CreateView):
+    form_class = UserForm
+    success_url = reverse_lazy("login")
+    template_name = "users/registration.html"
+    #extra_context = {'title': gettext_lazy('Create user')}
+    success_message = gettext_lazy('User created successfully')
 
+
+class ListUsers(ListView):
+    model = User
+    context_object_name = 'users'
+    extra_context = {'title': gettext_lazy('Users')}
+
+
+
+
+class UpdateUser(SuccessMessageMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('home_users')
+    success_message = gettext_lazy('User successfully changed')
+
+
+class DeleteUser(SuccessMessageMixin, DeleteView):
+    model = User
+    success_url = reverse_lazy('home_users')
+    success_message = gettext_lazy('User successfully deleted')
