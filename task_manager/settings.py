5573598cfe059c50b4d3af9e51963ab4d0a8968c
Marvv1ne <gorn89@inbox.ru>
2025-04-13 13:27:24 +0400

    feat: add AUTH_PASSWORD_VALIDATORS setting with min length = 3

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.AppUser'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 3,
+        }
+    },
+]
