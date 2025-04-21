3e8748466d5a1027f44693670e4b0b4e9f6f06fa
Marvv1ne <gorn89@inbox.ru>
2025-04-21 20:57:07 +0400

    feat: make test for update user

--- users/tests.py
+++ users/tests.py
@@ -6,11 +6,12 @@ from django.utils.translation import gettext_lazy as _
 from users.models import AppUser
 
 
+
 class SignUpPageTests(TestCase):
     def setUp(self) -> None:
-        self.first_name = 'Test_first_name'
-        self.second_name = 'Test_second_name'
-        self.username = 'testuser'
+        self.first_name = 'First_name'
+        self.last_name = 'Last_name'
+        self.username = 'test_user'
         self.password = 'password'
 
     def test_signup_page_url(self):
@@ -26,13 +27,48 @@ class SignUpPageTests(TestCase):
     def test_signup_form(self):
         response = self.client.post(reverse('signup'), data={
             'first_name': self.first_name,
-            'second_name': self.second_name,
+            'last_name': self.last_name,
             'username': self.username,
             'password1': self.password,
             'password2': self.password
         })
         self.assertEqual(response.status_code, 302)
-
     
+    
+
+
+class TestUpdateDeleteUser(TestCase):
+    def setUp(self):
+        self.data = {
+        'first_name': 'First_name',
+        'last_name': 'Last_name',
+        'username': 'test_username',
+        'password': 'password',
+    }
+        self.test_user = AppUser.objects.create_user(**self.data)
+        
+    def test_login_user(self):
+        response = self.client.post(reverse('signin'), data={
+            'username': self.data['username'],
+            'password': self.data['password'],
+            
+        })
+        self.assertEqual(response.status_code, 302)
+    
+    def test_update_form(self):
+        id = AppUser.objects.get(username='test_username').pk
+        response = self.client.post(reverse('update_user', id), data={
+            'first_name': 'Another_first_name',
+            'last_name': 'Another_last_name',
+            'username': 'another_username',
+            'password1': 'password',
+            'password2': 'password'
+        })
+        self.assertEqual(response.status_code, 302)
+    
+    
+    
+    
+        
 
 
