38bedcd536b658ddaaf23766aacd4dacc8fad49f
Marvv1ne <gorn89@inbox.ru>
2025-04-28 10:29:54 +0400

    fix: fix localization in productions settings

--- task_manager/settings/production.py
+++ task_manager/settings/production.py
@@ -21,10 +21,5 @@ DATABASES = {
              'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
              }
 
-LOCALE_PATHS = (
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__), 
-                '..', "locale")),
-)
-print(os.environ.get('DATABASE_URL'))
+LOCALE_PATHS = ("locale",)
+
