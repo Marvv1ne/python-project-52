e65e4fe6ed1231118d27b163ffe7a7844debcc5a
Marvv1ne <gorn89@inbox.ru>
2025-04-29 16:09:34 +0400

    fix: add default db sqlite

--- task_manager/settings/production.py
+++ task_manager/settings/production.py
@@ -18,7 +18,14 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 DATABASES = {
-             'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600),
-             }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 LOCALE_PATHS = ("locale",)
+print(dj_database_url.parse(os.getenv('DATABASE_URL')))
