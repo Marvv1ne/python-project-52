8542819a101d3301701c617621d5205559bb3592
Marvv1ne <gorn89@inbox.ru>
2025-04-29 18:28:40 +0400

    fix: improve linter comment

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class AppUser(AbstractUser):
     USERNAME_FIELD = 'username'
-    
+
     def __str__(self):
         return self.get_full_name()
